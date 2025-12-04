c2a202ec569921f6ed4fea5ade4df9a28446fad1
salman-khan125 <salmankhaneul@gmail.com>
2025-12-04 15:13:06 -0800

    Fix case and duplicate props for Vercel

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -37,7 +37,7 @@ const Services = () => {
         container
         spacing={0}
         alignItems="center"
-        justifyContent="center"
+      
         justifyContent="space-between"
       >
         {/* LEFT SIDE — Phone Image */}
